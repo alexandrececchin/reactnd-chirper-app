1eef8b94bb77c406425158751cd64c1afa02c8c3
Alexandre Rocha <alexandrececchin@gmail.com>
2019-01-01 21:37:01 -0200

    Create Tweet page

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 import Dashboard from './Dashboard';
 import NewTweet from "./NewTweet";
+import TweetPage from "./TweetPage";
 import LoadingBar from "react-redux-loading";
+import Nav from "./Nav";
 
 class App extends Component {
   componentDidMount() {
@@ -12,7 +15,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        {this.props.loading ? <LoadingBar /> : <NewTweet />}
+        {this.props.loading ? <LoadingBar /> : <TweetPage match={{ params: { id: '8xf0y6ziyjabvozdd253nd' } }} />}
       </div>
     )
   }
